3eebf826f4cf38c0d2c544cfcf19eee17949442d
Daisuke Nakahara <daisuken@msn.com>
2021-09-15 14:33:45 -0700

    Run upgrade

--- upgrade/scripts/2.Upgrade.sh
+++ upgrade/scripts/2.Upgrade.sh
@@ -3,4 +3,5 @@ apt update && \
 apt upgrade -y && \
 apt dist-upgrade -y && \
 apt autoremove -y && \
-apt install -y update-manager-core
+apt install -y update-manager-core && \
+do-release-upgrade -f DistUpgradeViewNonInteractive
